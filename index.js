dfa12f2b6411133cb7ad217a22b4adb90c3d1f95
Manisha.V <128169122+Manisha-Velliangiri@users.noreply.github.com>
2023-06-30 01:05:59 +0530

    This is a website for rolling dice.
    This generates a random number and decides the winner! .Helpful when you have disputes with your sibling, on who is getting the last piece of cake.

---
+++ index.js
@@ -0,0 +1,31 @@
+//generating random number
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+// generating the image name
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+// providing source for the generated image
+var randomImageSource = "./" + randomDiceImage; //images/dice1.png - images/dice6.png
+//selecting the image acc to the random img name generated
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+// doing the same for the 2nd dice
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "./dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
+
+
